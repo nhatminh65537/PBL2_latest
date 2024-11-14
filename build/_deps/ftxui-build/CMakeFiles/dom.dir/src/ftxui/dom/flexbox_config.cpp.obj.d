2587a0c379294245b6c6d9a2f4592861e5ee61b0
longqt321 <longqt321@gmail.com>
2024-11-14 19:12:20 +0700

    delete unnecessary folders

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj: \
- D:\code_and_study\DUT\PBL2_Clion\build\_deps\ftxui-src\src\ftxui\dom\flexbox_config.cpp \
- D:/code_and_study/DUT/PBL2_Clion/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
