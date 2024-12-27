403ecf28a0b18ced369524e0ba9e51f87c123df3
thainhatminh2005 <61194805+nhatminh65537@users.noreply.github.com>
2024-12-28 00:03:37 +0700

    For merge

--- main.cpp
+++ main.cpp
@@ -2,17 +2,26 @@
 #include <fstream>
 #include <iostream>
 #include <functional>
+#include <csignal> 
 #include "ui.h"
 #include "Salon.h"
 
 std::function<void()> currentScreen = screenWelcome;
 
+void signal_handler(int signal_num) 
+{ 
+    currentScreen = nullptr;
+    exit(0);
+} 
+
 int main(){
     freopen("log.txt","w", stderr);
-    std::cerr << Hash("pass");
+    signal(SIGBREAK, signal_handler);
+
     try {
         while(currentScreen) currentScreen();
     } catch (int code) {
+        std::cerr << "Exit main with code " << code << std::endl;
         return code;
     } catch (const char* msg) {
         std::cerr << msg << std::endl;
